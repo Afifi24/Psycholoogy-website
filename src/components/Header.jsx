8da78f2b8e46388b656e9a3beadcc9600e8a04c2
afifi brahim <afifi71brahim@gmail.com>
2023-05-19 16:52:21 +0100

    add mobile navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import {MdOutlineKeyboardArrowDown} from 'react-icons/md'
 const Header = () => {
     const [isopen,setIsopen] = useState(false)
     const [show,setShow] = useState(false)
+    const [pages,setPages] = useState(false)
     const useref = useRef()
     const closenav = (e)=>{
         if(useref.current == e.target){
@@ -24,61 +25,139 @@ const Header = () => {
                 <img className='w-48' src='https://assets.website-files.com/6399ec1545e218ece386c9b6/639a0d680ebba097d7bc57b5_Psychology-logo-white.svg' alt="" />
                 
             </div>
-            <ul className=' hidden lg:flex  items-center gap-10'>
-                <li onMouseOver={Showhide}  className='text-white  relative text-lg '>
-                  <p className=' cursor-pointer'>Demos</p>
+            <ul className=' hidden lg:flex  items-center gap-0'>
+                <li onMouseEnter={Showhide} onMouseLeave={Showhide} className='text-white  p-[20px] z-50   relative text-lg '>
+                  <p className=' hover:text-[#FFCBC7] cursor-pointer'>Demos</p>
                 {
-                  show &&  <div className='bg-white rounded-xl p-6 absolute top-6 text-black flex flex-col gap-2'>
-                  <a className='cursor-pointer' href="">home1</a>
-                  <a className='cursor-pointer' href="">home2</a>
+                  show &&  <div className='bg-white text-[15px] rounded-xl p-4 min-w-[150px] absolute top-16 text-black flex flex-col gap-2'>
+                 <div className='flex group items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href=""> home1</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href=""> home2</a>
+                 </div>
+                 <div className='w-5 h-5 bg-white absolute  -top-2 rotate-45'></div>
                   </div>
                 }
                 
                 </li>
                
                 
-                <li className='text-white text-lg '>About</li>
-                <li className='text-white text-lg '>Sessions</li>
-                <li className='text-white text-lg '>Contact Us</li>
-                <li className='text-white text-lg '>All pages</li>
+                <li className='text-white p-[20px] text-lg hover:text-[#FFCBC7] duration-200 cursor-pointer '>About</li>
+                <li className='text-white p-[20px] text-lg hover:text-[#FFCBC7] duration-200 cursor-pointer '>Sessions</li>
+                <li className='text-white p-[20px] text-lg hover:text-[#FFCBC7] duration-200 cursor-pointer '>Contact Us</li>
+                <li onMouseEnter={()=>setPages(true)} onMouseLeave={()=>setPages(false)} className='text-white  p-[20px] z-[100]   relative text-lg '>
+                  <p className=' hover:text-[#FFCBC7] cursor-pointer'>All pages</p>
+                {
+                  pages && 
+                  
+                <div className='bg-white rounded-xl p-6 min-w-[380px] absolute top-16 text-black flex items-center gap-12'>
+                <div className='text-[15px]'>
+                <div className='flex group items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href=""> Home1</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href=""> Home2</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">About Us</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">Our Team</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">Team Member</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">Pricing </a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">FAQ </a>
+                 </div>
+                </div>
+                  <div className='text-[15px]'>
+                  <div className='flex group items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href=""> Contact Us</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href=""> Testimonials</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">Sessions</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">Sessions Details</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">Blog </a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">Blog Details</a>
+                 </div>
+                 <div className='flex group duration-200 items-center gap-2'>
+                 <p className='w-0 duration-200 group-hover:w-5  rounded-full h-[2px] bg-red-500'></p>
+                 <a className='cursor-pointer hover:text-red-500 duration-200' href="">Shop </a>
+                 </div>
+                  </div>
+                 <div className='w-5 h-5 bg-white absolute  -top-2 rotate-45'></div>
+                  </div>
+                }
+                
+                </li>
             </ul>
             <button className=' hidden lg:flex  rounded-full text-white   hover:text-[#FF5142] duration-200 border hover:bg-white text-lg py-5 px-8'>Book a consultation</button>
             <div onClick={()=>setIsopen(!isopen)} className='lg:hidden z-50 cursor-pointer text-white text-4xl'>
                 <AiOutlineBars className={` ${isopen ? 'text-black':''}`}/>
                 
             </div>
+            {/* mobile menu */}
             {
                 isopen &&
-               <div onClick={closenav} ref={useref} className={`absolute ${isopen ? '-translate-x-0 duration-500':'-translate-x-full duration-500'} w-full h-full min-h-screen left-0 top-0 `}>
-                 <div className={`lg:hidden ${isopen ? '-translate-x-0 duration-500':'-translate-x-full duration-500'}  absolute top-0 left-0 h-full min-h-screen px-6 py-6 w-2/3 bg-white`} id='mobile'>
+               <div onClick={closenav} ref={useref} className={`absolute ${isopen ? '-translate-x-0 duration-500':'-translate-x-full duration-500'} w-full z-[100] h-full min-h-screen left-0 top-0 `}>
+                 <div className={`lg:hidden ${isopen ? '-translate-x-0 duration-500':'-translate-x-full duration-500'}  absolute top-0 left-0 h-full min-h-screen  py-6 w-2/3 bg-white`} id='mobile'>
                      <div className='flex items-center'>
                         <h1 className='font-bold text-xl uppercase'>psychology</h1>
                      <MdOutlineClose onClick={()=>setIsopen(!isopen)} className='text-4xl cursor-pointer absolute right-4 top-6'/>
 
                      </div>
 
-                     <div className='mt-10 flex flex-col gap-4'>
-                            <div onMouseEnter={Showhide} onMouseLeave={Showhide} className='flex flex-col gap-4'>
-                                <div href='' className=' flex justify-between items-center cursor-pointer'  >
+                     <div className='mt-10 flex flex-col'>
+                            <div onMouseEnter={Showhide} onMouseLeave={Showhide} className='flex  flex-col gap-4'>
+                                <div href='' className=' flex p-[10px] justify-between items-center cursor-pointer'  >
                                   
-                                <p onMouseEnter={Showhide} onMouseLeave={Showhide}>  Demos</p>
+                                <p>  Demos</p>
                                 <MdOutlineKeyboardArrowDown className={`${show ? 'rotate-180 duration-200' :' duration-200'}`}/>
                                   
                                   
                                   </div>
                                 { show &&
-                                    <div className='flex pl-4 flex-col gap-3'>
+                                    <div className='flex pl-6 flex-col gap-3'>
                                     <a className='hover:text-red-500' href="">Home1</a>
                                     <a className='hover:text-red-500' href="">Home2</a>
                                 </div>}
                             </div>
 
-                             <div className='flex flex-col gap-4'>
-                             <a className='cursor-pointer duration-200 hover:text-red-500'>About</a>
-                             <a className='cursor-pointer duration-200 hover:text-red-500'>Sessions</a>
-                             <a className='cursor-pointer duration-200 hover:text-red-500'>Contact Us</a>
+                             <div className='flex flex-col'>
+                             <a className='cursor-pointer p-[10px] duration-200 hover:text-red-500'>About</a>
+                             <a className='cursor-pointer p-[10px] duration-200 hover:text-red-500'>Sessions</a>
+                             <a className='cursor-pointer p-[10px] duration-200 hover:text-red-500'>Contact Us</a>
                              </div>
-                             <div>
+                             <div className='p-[10px] hover:text-red-500 duration-200 cursor-pointer'>
                                 <a>All pages</a>
                              </div>
                      </div>
@@ -93,16 +172,13 @@ const Header = () => {
                <div className='flex flex-col text-white justify-center gap-8'>
                <h1 className=' text-5xl lg:text-6xl leading-[48px] lg:leading-[70px] font-bold'>Your Mental <br /> Health Matters</h1>
                <p className='text-lg'>I help people across the country and around the world create and sustain loving and resilient relationship.</p>
-               <button className=' w-fit  rounded-full hover:text-white bg-white  text-[#615c5c] duration-200 border hover:bg-transparent text-lg py-5 px-8'>Book a consultation</button>
+               <button className=' w-fit  rounded-full z-50 hover:text-white bg-white  text-[#615c5c] duration-200 border hover:bg-transparent text-lg py-5 px-8'>Book a consultation</button>
                </div>
                <div className='w-full'>
                 <img className=' lg:absolute z-50 lg:w-1/2 xl:w-full lg:right-3 lg:-bottom-2 xl:relative'  src="https://assets.website-files.com/6399ec1545e218ece386c9b6/639cb57a567c9cb407a525ae_My%20project%201.webp" alt="" />
                </div>
-                
                 <img className='absolute image1-animation w-1/2 md:w-1/3 top-4 left-2' src="https://assets.website-files.com/6399ec1545e218ece386c9b6/639ca9553510a31b2c95753b_Vector%2024.svg" alt="" />
-                <div className='absolute hidden lg:block image2-animation    left-0 w-1/3 -bottom-48 md:-bottom-10 md:w-1/4'>
-                <img className='w-full ' src="https://assets.website-files.com/6399ec1545e218ece386c9b6/639ca955617149547d8b36c3_Vector%2023.svg" alt="" />
-                </div>
+                <img className=' absolute hidden lg:block image2-animation    left-0 w-1/3 -bottom-48 md:-bottom-10 md:w-1/4 ' src="https://assets.website-files.com/6399ec1545e218ece386c9b6/639ca955617149547d8b36c3_Vector%2023.svg" alt="" />
                 <img className='absolute w-full top-1/2 md:top-40 -right-10 image3-animation md:w-1/2' src="https://assets.website-files.com/6399ec1545e218ece386c9b6/639ca955155635f6990e12b1_Vector%2022.svg" alt="" />
                 <img className='absolute image2-animation hidden lg:block bottom-0  -right-20 md:w-[50%]' src="	https://assets.website-files.com/6399ec1545e218ece386c9b6/639ca9553510a3d50195753a_Vector%2025.svg" alt="" />
              </div>
